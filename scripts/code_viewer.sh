2af856bc46f8ff4874901163b86286c2e1700223
Tetsuro Ando <tetsuro997@gmail.com>
2025-02-25 14:49:15 +0900

    add scriptfile codeviewer.sh

---
+++ scripts/code_viewer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TMP_OUTPUT=$(mktemp)
+
+remove_comments() {
+    local input_file="$1"
+    local relative_path="${input_file#./}"
+    
+    # libftディレクトリはスキップ
+    if [[ "$input_file" == *"/lib/libft/"* ]]; then
+        return
+    fi
+    
+    {
+        echo "----------------------------------------"
+        echo "File: $relative_path"
+        echo "----------------------------------------"
+        sed '/\/\*.*\*\//d; /\/\*.*/,/.*\*\//d; s/\/\/.*//' "$input_file" | awk 'NF'
+        echo "----------------------------------------"
+        echo
+    } >> "$TMP_OUTPUT"
+}
+
+tree >> "$TMP_OUTPUT"
+find src tests Makefile -type f \( -name "*.c" -o -name "*.h" -o -name "Makefile" \) | while read -r file; do
+    remove_comments "$file"
+done
+
+cat "$TMP_OUTPUT" | pbcopy
+rm "$TMP_OUTPUT"
